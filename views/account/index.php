d792336bb6f5e65946748b1f565f335a8a344120
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-07-30 15:16:59 +0000

    Accounts GridView done

--- views/account/index.php
+++ views/account/index.php
@@ -9,31 +9,40 @@ use hipanel\modules\hosting\grid\AccountGridView;
 use hipanel\widgets\ActionBox;
 use hipanel\widgets\Pjax;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title                    = Yii::t('app', 'Accounts');
-$this->params['breadcrumbs'][]  = $this->title;
+$this->breadcrumbs->setItems([
+    $this->title,
+]);
 $this->params['subtitle']       = Yii::$app->request->queryParams ? 'filtered list' : 'full list';
 
 Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true]));
 
-$box = ActionBox::begin(['options' => ['class' => 'box-info']]);
+echo Html::beginForm();
+$box = ActionBox::begin(['bulk' => true, 'options' => ['class' => 'box-info']]);
 $box->beginActions();
 echo Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => 'account']), ['create'], ['class' => 'btn btn-success']) . '&nbsp;';
 echo Html::a(Yii::t('app', 'Create FTP {modelClass}', ['modelClass' => 'account']), ['create-ftponly'], ['class' => 'btn btn-success']) . '&nbsp;';
+echo '&nbsp;';
 $box->endActions();
+
+$box->beginBulkActions();
+echo Html::submitButton(Yii::t('app', 'Delete'), ['class' => 'btn btn-danger', 'formmethod' => 'POST', 'formaction' => Url::to('delete')]);
+$box->endBulkActions();
 $box::end();
 
 echo AccountGridView::widget([
     'dataProvider' => $dataProvider,
     'filterModel'  => $searchModel,
     'columns'      => [
-        'checkbox',
         'seller_id',
         'client_id',
         'server',
         'account',
         'state',
-        'actions'
+        'actions',
+        'checkbox',
     ],
 ]);
 
