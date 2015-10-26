03d060ecaedb146ec3bb45e36359a82ba53634a9
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-10-26 17:56:21 +0200

    minor

--- src/views/account/index.php
+++ src/views/account/index.php
@@ -7,62 +7,65 @@
 
 use hipanel\modules\hosting\grid\AccountGridView;
 use hipanel\widgets\ActionBox;
-use hipanel\widgets\LinkSorter;
 use hipanel\widgets\Pjax;
-use yii\helpers\Html;
-use yii\helpers\Url;
 
-$this->title                    = Yii::t('app', 'Accounts');
+$this->title = Yii::t('app', 'Accounts');
 $this->breadcrumbs->setItems([
     $this->title,
 ]);
-$this->params['subtitle']       = array_filter(Yii::$app->request->get($model->formName(), [])) ? 'filtered list' : 'full list'; ?>
+$this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? 'filtered list' : 'full list'; ?>
 
 <?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])); ?>
 
 <?php $box = ActionBox::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
-    <?php $box->beginActions() ?>
-        <div class="dropdown">
-            <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                <?= Yii::t('app', 'Create account'); ?>
-                <span class="caret"></span>
-            </button>
-            <?= \yii\bootstrap\Dropdown::widget([
-                'items' => [
-                    ['label' => Yii::t('app', 'Create account'), 'url' => ['create']],
-                    ['label' => Yii::t('app', 'Create FTP account'), 'url' => ['create-ftponly']],
-                ]
-            ]); ?>
-            <?= $box->renderSearchButton() ?>
-            <?= $box->renderSorter([
-                'attributes' => [
-                    'login', 'client', 'seller', 'server', 'state', 'type'
-                ],
-            ]) ?>
-            <?= $box->renderPerPage() ?>
-        </div>
-    <?php $box->endActions() ?>
-    <?php $box->beginBulkActions() ?>
-
-        <?= $box->renderDeleteButton() ?>
-    <?php $box->endBulkActions() ?>
-        <?= $box->renderSearchForm(['stateData' => $stateData, 'typeData' => $typeData]) ?>
-    <?php $box->end() ?>
-
-    <?php $box->beginBulkForm() ?>
-        <?= AccountGridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel'  => $model,
-            'columns'      => [
-                'checkbox',
-                'account',
+<?php $box->beginActions() ?>
+    <div class="dropdown">
+        <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="true">
+            <?= Yii::t('app', 'Create account'); ?>
+            <span class="caret"></span>
+        </button>
+        <?= \yii\bootstrap\Dropdown::widget([
+            'items' => [
+                ['label' => Yii::t('app', 'Create account'), 'url' => ['create']],
+                ['label' => Yii::t('app', 'Create FTP account'), 'url' => ['create-ftponly']],
+            ]
+        ]); ?>
+        <?= $box->renderSearchButton() ?>
+        <?= $box->renderSorter([
+            'attributes' => [
+                'login',
                 'client',
                 'seller',
                 'server',
                 'state',
-                'type',
-                'actions',
+                'type'
             ],
         ]) ?>
-    <?php $box->endBulkForm() ?>
+        <?= $box->renderPerPage() ?>
+    </div>
+<?php $box->endActions() ?>
+<?php $box->beginBulkActions() ?>
+
+<?= $box->renderDeleteButton() ?>
+<?php $box->endBulkActions() ?>
+<?= $box->renderSearchForm(['stateData' => $stateData, 'typeData' => $typeData]) ?>
+<?php $box->end() ?>
+
+<?php $box->beginBulkForm() ?>
+<?= AccountGridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $model,
+    'columns' => [
+        'checkbox',
+        'account',
+        'client',
+        'seller',
+        'server',
+        'state',
+        'type',
+        'actions',
+    ],
+]) ?>
+<?php $box->endBulkForm() ?>
 <?php Pjax::end();
