4abd9ef7f327fab54fae314616fcbb0493063f84
Andrey Klochok <andreyklochok@gmail.com>
2015-11-26 16:58:09 +0200

    Add view file to Request

--- src/views/request/view.php
+++ src/views/request/view.php
@@ -9,24 +9,27 @@ use hipanel\modules\hosting\grid\RequestGridView;
 use hipanel\widgets\Pjax;
 use yii\helpers\Html;
 
-$this->title                   = Html::encode($model->domain);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Domains'), 'url' => ['index']];
+$this->title = Html::encode(Yii::t('app', 'Request') . ' #' . $model->id);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Requests'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<? Pjax::begin(Yii::$app->params['pjax']) ?>
 <div class="row">
 
-<div class="col-md-4">
-    <?= RequestGridView::detailView([
-        'model'   => $model,
-        'columns' => [
-            'seller_id','client_id',
-            ['attribute' => 'request'],
-        ],
-    ]) ?>
-</div>
+    <div class="col-md-4">
+        <?= RequestGridView::detailView([
+            'model' => $model,
+            'columns' => [
+                'classes',
+                'server',
+                'account',
+                'object',
+                'time',
+                'state',
+            ],
+        ]) ?>
+    </div>
 
 </div>
-<?php Pjax::end() ?>
+
