4af676c7a7fad4bb0ef288f066e6175b4ec7980b
tofid <andreyklochok@gmail.com>
2016-11-15 17:39:39 +0200

    Removed actions column

--- src/views/request/index.php
+++ src/views/request/index.php
@@ -42,7 +42,6 @@ $this->params['breadcrumbs'][]  = $this->title;
                         'checkbox', 'classes',
                         'server', 'account',
                         'object', 'time', 'state',
-                        'actions',
                     ],
                 ]) ?>
             <?php $page->endBulkForm() ?>
