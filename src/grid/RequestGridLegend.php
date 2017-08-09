348f46c9c601e445687dc6188fa01af57c7abf7b
tofid <andreyklochok@gmail.com>
2017-08-09 19:12:03 +0300

    Updated RequestGridLegend

--- src/grid/RequestGridLegend.php
+++ src/grid/RequestGridLegend.php
@@ -12,36 +12,52 @@ class RequestGridLegend extends BaseGridLegend implements GridLegendInterface
     {
         return [
             [
+                'label' => Yii::t('hipanel:hosting', 'Scheduled time:'),
+                'tag' => 'h4',
+            ],
+            [
                 'label' => Yii::t('hipanel:hosting', 'Already'),
                 'color' => '#E0E0E0',
+                'rule' => false,
+                'columns' => ['time'],
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'Deferred'),
                 'color' => '#AAAAFF',
-                'rule' => $this->model->type === 'free',
+                'rule' => false,
+                'columns' => ['time'],
+            ],
+            [
+                'label' => Yii::t('hipanel:hosting', 'State:'),
+                'tag' => 'h4',
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'New'),
                 'color' => '#FFFF99',
                 'rule' => $this->model->state === 'new',
+                'columns' => ['state'],
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'In progress'),
                 'color' => '#AAFFAA',
                 'rule' => $this->model->state === 'progress',
+                'columns' => ['state'],
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'Done'),
+                'columns' => ['state'],
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'Error'),
                 'color' => '#FFCCCC',
                 'rule' => $this->model->state === 'error',
+                'columns' => ['state'],
             ],
             [
                 'label' => Yii::t('hipanel:hosting', 'Buzzed'),
                 'color' => '#FFCCCC',
                 'rule' => $this->model->state === 'buzzed',
+                'columns' => ['state'],
             ],
         ];
     }
