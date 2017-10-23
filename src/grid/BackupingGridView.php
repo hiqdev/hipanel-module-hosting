f2d522289d2f890d921ec5f6fc5ef89f439f3d85
Andrii Vasyliev <sol@hiqdev.com>
2017-10-23 11:27:31 +0000

    fixed typo

--- src/grid/BackupingGridView.php
+++ src/grid/BackupingGridView.php
@@ -18,7 +18,7 @@ namespace hipanel\modules\hosting\grid;
 
 use hipanel\grid\MainColumn;
 use hipanel\helpers\Url;
-use hipanel\widgets\obj\ObjLinkWidget;
+use hipanel\widgets\obj\ObjLabelWidget;
 use hipanel\modules\server\grid\ServerColumn;
 use hiqdev\xeditable\widgets\XEditable;
 use Yii;
