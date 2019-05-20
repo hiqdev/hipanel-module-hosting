1543751616b87de0c70a206bf836ccb530394bf6
Andrey Klochok <andreyklochok@gmail.com>
2019-05-20 13:21:04 +0300

    Removed `Expand` menu item from IP Actions Menu

--- src/menus/IpActionsMenu.php
+++ src/menus/IpActionsMenu.php
@@ -34,34 +34,12 @@ class IpActionsMenu extends \hiqdev\yii2\menus\Menu
                 'icon' => 'fa-info',
                 'url' => ['@ip/view', 'id' => $this->model->id],
             ],
-            'expand' => [
-                'label' => Yii::t('hipanel:hosting', 'Expand'),
-                'icon' => 'fa-th',
-                'url' => ['@ip/expand', 'id' => $this->model->id],
-                'linkOptions' => [
-                    'class' => 'btn-expand-ip',
-                    'data-id' => $this->model->id,
-                    'data-pjax' => 0,
-                ],
-            ],
             'update' => [
                 'label' => Yii::t('hipanel', 'Update'),
                 'icon' => 'fa-pencil',
                 'url' => ['@ip/update', 'id' => $this->model->id],
                 'visible' => Yii::$app->user->can('admin'),
             ],
-//            'delete' => [
-//                'label' => Yii::t('hipanel', 'Delete'),
-//                'icon' => 'fa-trash',
-//                'url' => ['@ip/delete', 'id' => $this->model->id],
-//                'linkOptions' => [
-//                    'data' => [
-//                        'confirm' => Yii::t('hipanel', 'Are you sure you want to delete this item?'),
-//                        'method' => 'POST',
-//                        'pjax' => '0',
-//                    ],
-//                ],
-//            ],
         ];
     }
 
