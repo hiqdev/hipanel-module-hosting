4371e71f340d76eb77ac5602eb6ff1dbe032aae0
Yurii Myronchuk <bladeroot@hiqdev.com>
2020-08-05 13:20:10 +0000

    rollback module.hosting.is_public

--- src/menus/SidebarMenu.php
+++ src/menus/SidebarMenu.php
@@ -96,7 +96,7 @@ class SidebarMenu extends \hiqdev\yii2\menus\Menu
             }
         }
 
-        $menu['hosting']['visible'] = $menu['hosting']['visible'] || $user->can('dns.read');
+        $menu['hosting']['visible'] = Yii::$app->params['module.hosting.is_public'] && ($menu['hosting']['visible'] || $user->can('dns.read'));
 
         return $menu;
     }
