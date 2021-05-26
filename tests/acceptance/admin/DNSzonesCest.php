981846006d90b8a5c97b641b270a69932e4592fa
Mark <83531949+Drakorgaur@users.noreply.github.com>
2021-05-26 23:15:35 +0300

    Added DNSzonesCest (#52)
    * Added DNSzonesCest
    
    * Update DNSzonesCest.php

---
+++ tests/acceptance/admin/DNSzonesCest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace hipanel\modules\hosting\tests\acceptance\admin;
+
+use hipanel\helpers\Url;
+use hipanel\tests\_support\Page\IndexPage;
+use hipanel\tests\_support\Page\Widget\Input\Select2;
+use hipanel\tests\_support\Page\Widget\Input\Input;
+use hipanel\tests\_support\Step\Acceptance\Admin;
+
+class DNSzoneCest
+{
+    /**
+     * @var IndexPage
+     */
+    private $index;
+
+    public function _before(Admin $I)
+    {
+        $this->index = new IndexPage($I);
+    }
+
+    public function ensureIndexPageWorks(Admin $I)
+    {
+        $I->login();
+        $I->needPage(Url::to('@dns/zone/index'));
+        $I->see('DNS zones', 'h1');
+        $this->ensureICanSeeAdvancedSearchBox($I);
+        $this->ensureICanSeeBulkSearchBox();
+    }
+
+    private function ensureICanSeeAdvancedSearchBox(Admin $I)
+    {
+        $this->index->containsFilters([
+            Input::asAdvancedSearch($I, 'Domain'),
+            Select2::asAdvancedSearch($I, 'Account'),
+            Select2::asAdvancedSearch($I, 'Client'),
+            Select2::asAdvancedSearch($I, 'Reseller'),
+        ]);
+    }
+
+    private function ensureICanSeeBulkSearchBox()
+    {
+        $this->index->containsBulkButtons([
+            'Export DNS records',
+        ]);
+        $this->index->containsColumns([
+            'Client',
+            'Domain',
+            'NS servers',
+            'DNS',
+            'Bound to',
+        ]);
+    }
+}
