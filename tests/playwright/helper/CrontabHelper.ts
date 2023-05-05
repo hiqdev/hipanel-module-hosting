dfcdb4910f014d3452005c632d602213db19b617
gadezist <andrybitko@gmail.com>
2023-05-05 15:37:58 +0300

    HP-1352 HiAPI crontab module rewrite

---
+++ tests/playwright/helper/CrontabHelper.ts
@@ -0,0 +1,31 @@
+import { expect, Locator, Page } from "@playwright/test";
+import Index from "@hipanel-core/page/Index";
+import Alert from "@hipanel-core/ui/Alert";
+
+export default class CrontabHelper {
+    private page: Page;
+    private index: Index;
+
+    public constructor(page: Page) {
+        this.page = page;
+        this.index = new Index(page);
+    }
+
+    async gotoIndexCrontab() {
+        await this.page.goto('/hosting/crontab/index');
+        await expect(this.page).toHaveTitle("Crons");
+    }
+
+    async gotoCrontabPage(row: number) {
+        await this.page.locator(`//tr[${row}]//td[1]//a[1]`).click();
+    }
+
+    async checkDetailViewData(crontab: object) {
+        await expect(this.page.locator('//table[contains(@class, "detail-view")]//tbody/tr[1]/td')).toContainText(crontab['account']);
+        await expect(this.page.locator('//table[contains(@class, "detail-view")]//tbody/tr[2]/td')).toContainText(crontab['server']);
+    }
+
+    async seeSuccessAlert(message: string) {
+        await Alert.on(this.page).hasText(message);
+    }
+}
