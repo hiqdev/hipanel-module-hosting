ad35c19bd222a3b615201b73bcb239f98f412917
Andrii Klochok <andreyklochok@gmail.com>
2023-10-09 20:16:01 +0300

    tests

--- tests/playwright/e2e/admin/ip/index.spec.ts
+++ tests/playwright/e2e/admin/ip/index.spec.ts
@@ -3,12 +3,7 @@ import { expect } from "@playwright/test";
 import IPHelper from "@hipanel-module-hosting/helper/IPHelper";
 import Index from "@hipanel-core/page/Index";
 
-const ip: object = {
-  ip: '127.0.0.199',
-  links: 'DSTEST02',
-};
-
-test("Correct view IP @hipanel-module-hosting @admin", async ({ adminPage }) => {
-  const ipHelper = new IPHelper(adminPage);
-  await ipHelper.gotoIndexIP();
-});
+// test("Correct view IP @hipanel-module-hosting @admin", async ({ adminPage }) => {
+//   const ipHelper = new IPHelper(adminPage);
+//   await ipHelper.gotoIndexIP();
+// });
